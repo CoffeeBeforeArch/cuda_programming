dc2641b5f496a4e96736c80c729f4965446ef3ab
CoffeeBeforeArch <CoffeeBeforeArch@gmail.com>
2019-03-04 00:18:56 -0500

    Add new sum reduction example that removes expensive mod operator and uses sequential threads

---
+++ sum_reduction_bank_conflicts/sum_reduction_bank_conflicts/sum_reduction_bank_conflicts.cu
@@ -0,0 +1,94 @@
+// This program performs sum reduction with an optimization
+// removing warp divergence
+// By: Nick from CoffeeBeforeArch
+
+#define SIZE 256
+#define SHMEM_SIZE 256 * 4
+
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+
+__global__ void sum_reduction(int *v, int *v_r) {
+	// Allocate shared memory
+	__shared__ int partial_sum[SHMEM_SIZE];
+
+	// Calculate thread ID
+	int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+	// Load elements into shared memory
+	partial_sum[threadIdx.x] = v[tid];
+	__syncthreads();
+
+	// 
+	for (int s = 1; s < blockDim.x; s *= 2) {
+		// Change the indexing to be sequential threads
+		int index = 2 * s * threadIdx.x;
+
+		// Each thread does work unless the index goes off the block
+		if (index < blockDim.x) {
+			partial_sum[index] += partial_sum[index + s];
+		}
+		__syncthreads();
+	}
+
+	// Let the thread 0 for this block write it's result to main memory
+	// Result is inexed by this block
+	if (threadIdx.x == 0) {
+		v_r[blockIdx.x] = partial_sum[0];
+	}
+}
+
+void initialize_vector(int *v, int n) {
+	for (int i = 0; i < n; i++) {
+		v[i] = 1;//rand() % 10;
+	}
+}
+
+int main() {
+	// Vector size
+	int n = 1 << 16;
+	size_t bytes = n * sizeof(int);
+
+	// Original vector and result vector
+	int *h_v, *h_v_r;
+	int *d_v, *d_v_r;
+
+	// Allocate memory
+	h_v = (int*)malloc(bytes);
+	h_v_r = (int*)malloc(bytes);
+	cudaMalloc(&d_v, bytes);
+	cudaMalloc(&d_v_r, bytes);
+
+	// Initialize vector
+	initialize_vector(h_v, n);
+
+	// Copy to device
+	cudaMemcpy(d_v, h_v, bytes, cudaMemcpyHostToDevice);
+
+	// TB Size
+	int TB_SIZE = SIZE;
+
+	// Grid Size
+	int GRID_SIZE = (int)ceil(n / TB_SIZE);
+
+	// Call kernel
+	sum_reduction << <GRID_SIZE, TB_SIZE >> > (d_v, d_v_r);
+
+	sum_reduction << <1, TB_SIZE >> > (d_v_r, d_v_r);
+
+	// Copy to host;
+	cudaMemcpy(h_v_r, d_v_r, bytes, cudaMemcpyDeviceToHost);
+
+	// Print the result
+	printf("Accumulated result is %d \n", h_v_r[0]);
+	scanf("Press enter to continue: ");
+	assert(h_v_r[0] == 65536);
+
+	printf("COMPLETED SUCCESSFULLY\n");
+
+	return 0;
+}
