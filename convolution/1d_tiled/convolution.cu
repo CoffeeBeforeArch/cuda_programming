ea80a470f3e912fcdbf11d8f7d1e7855c7f8de9d
Nick <CoffeeBeforeArch@gmail.com>
2019-05-12 19:23:48 -0400

    Add working tiled example

--- convolution/1d_tiled/convolution.cu
+++ convolution/1d_tiled/convolution.cu
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <assert.h>
-
+#include <stdio.h>
 using namespace std;
 
 // Length of our convolution mask
@@ -38,17 +38,19 @@ __global__ void convolution_1d(int *array, int *result, int n){
     // Size of the padded shared memory array
     int n_padded = blockDim.x + d;
 
-    // Offset for the second set of loads
-    int offset = threadIdx.x + blockDim.x - r;
+    // Offset for the second set of loads in shared memory
+    int offset = threadIdx.x + blockDim.x;
+
+    // Global offset for the array in DRAM
+    int g_offset = blockDim.x * blockIdx.x + offset;
 
     // Load the lower elements first starting at the halo
     // This ensure divergence only once
-    s_array[threadIdx.x] = array[tid - r];
-    __syncthreads();
-
+    s_array[threadIdx.x] = array[tid];
+    
     // Load in the remaining upper elements
     if(offset < n_padded){
-        s_array[offset] = array[tid + offset];
+        s_array[offset] = array[g_offset];
     }
     __syncthreads();
 
@@ -88,26 +90,27 @@ int main(){
 
     // Radius for padding the array
     int r = MASK_LENGTH / 2;
+    int n_p = n + r * 2;
 
     // Size of the padded array in bytes
-    size_t bytes_p = (n + r * 2) * sizeof(int);
+    size_t bytes_p = n_p * sizeof(int);
 
     // Allocate the array (include edge elements)...
-    int *h_array = new int[n + r * 2];
+    int *h_array = new int[n_p];
 
     // ... and initialize it
-    for(int i = 0; i < (n + r * 2); i++){
-        if((i < r) || (i > n)){
+    for(int i = 0; i < n_p; i++){
+        if((i < r) || (i >= (n + r))){
             h_array[i] = 0;
         }else{
-            h_array[i] = rand() % 100;
+            h_array[i] = 1;//rand() % 100;
         }
     }
 
     // Allocate the mask and initialize it
     int *h_mask = new int[MASK_LENGTH];
     for(int i = 0; i < MASK_LENGTH; i++){
-        h_mask[i] = rand() % 10;
+        h_mask[i] = 1;//rand() % 10;
     }
 
     // Allocate space for the result
@@ -119,7 +122,7 @@ int main(){
     cudaMalloc(&d_result, bytes_n);
 
     // Copy the data to the device
-    cudaMemcpy(d_array, h_array, bytes_n, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_array, h_array, bytes_p, cudaMemcpyHostToDevice);
 
     // Copy the mask directly to the symbol
     // This would require 2 API calls with cudaMemcpy
@@ -133,7 +136,7 @@ int main(){
 
     // Amount of space per-block for shared memory
     // This is padded by the overhanging radius on either side
-    int SHMEM = THREADS + 2 * r;
+    size_t SHMEM = (THREADS + r * 2) * sizeof(int);
 
     // Call the kernel
     convolution_1d<<<GRID, THREADS, SHMEM>>>(d_array, d_result, n);
