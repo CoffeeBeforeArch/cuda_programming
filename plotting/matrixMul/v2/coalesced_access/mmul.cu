d43846162e074f3506a18d29765fa067fa106d29
Nick <CoffeeBeforeArch@gmail.com>
2019-08-25 11:32:33 -0400

    Ensure warps come from the same matrix rows

---
+++ plotting/matrixMul/v2/coalesced_access/mmul.cu
@@ -0,0 +1,73 @@
+// This program computes matrix multiplication
+// By: Nick from CoffeeBeforeArch
+
+#include <cstdlib>
+
+// Matrix Multiplication kernel
+// Optimizations:
+//  Accumulate partial results in a temporary variable
+//  Ensure all threads in warps access consecutive memory
+__global__ void matrixMul(float *a, float *b, float *c, int N){
+    // Calculate the row and column for each thread
+    int row = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Boundary check
+    if((row < N) && (col < N)){
+        // Each thread computes one element
+        int tmp = 0;
+        for(int i = 0; i < N; i++){
+            tmp += a[row * N + i] * b[i * N + col];
+        }
+
+        // Write back the tmp result
+        c[row * N + col] = tmp;
+    }
+}
+
+// Initialize a matrix with random numbers
+void init_matrix(float *m, int N){
+    for(int i = 0; i < N * N; i++){
+        m[i] = rand() % 100;
+    }
+}
+
+int main(){
+    // Problem size
+    int N = 1 << 14;
+    size_t bytes = N * N * sizeof(float);
+
+    // Allocate host memory (make sure C is zeroed)
+    float *h_a = (float*)malloc(bytes);
+    float *h_b = (float*)malloc(bytes);
+    float *h_c = (float*)calloc(N * N, sizeof(float));
+    
+    // Allocate device memory
+    float *d_a, *d_b, *d_c;
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
+
+    // Initialized host-side matrices
+    init_matrix(h_a, N * N);
+    init_matrix(h_b, N * N);
+
+    // Copy the matrices over
+    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_c, h_c, bytes, cudaMemcpyHostToDevice);
+
+    // Set up the CTA and Grid Dimensions
+    int threads = 32;
+    int blocks = (N + threads -1) / threads;
+    dim3 THREADS(threads, threads);
+    dim3 BLOCKS(blocks, blocks);
+
+    // Call our kernel
+    matrixMul<<<BLOCKS, THREADS>>>(d_a, d_b, d_c, N);
+    
+    // Copy data back to the host
+    cudaMemcpy(h_c, d_c, bytes, cudaMemcpyDeviceToHost);
+
+    return 0;
+}
